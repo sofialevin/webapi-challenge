a90b3d1e577b241b2b9abeb4edd5b1e6be3733ac
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-06 10:02:37 -0800

    Completed CRUD for projectsRouter.

---
+++ routes/actionsRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Actions = require('../data/helpers/actionModel');
+const Projects = require('../data/helpers/projectModel');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const server = express();
 
+const projectsRouter = require('./routes/projectsRouter.js');
+const actionsRouter = require('./routes/actionsRouter.js');
+
 server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>Hello from my API!</h2>`)
